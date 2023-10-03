4d860ced47f66cd82de76f91a9ccf260e1004fec
Alvian <alvian.1928@yahoo.com>
2023-10-03 16:55:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,37 @@
 // Toogle class active
 
-const navbarNav = document.querySelector('.navbar-nav');
-document.querySelector('#hamburger-menu').onclick = () => {
-  navbarNav.classList.toggle('active');
+const navbarNav = document.querySelector(".navbar-nav");
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
-
 // menghilangkan nav
-const hamburger = document.querySelector
-('#hamburger-menu');
+const hamburger = document.querySelector("#hamburger-menu");
 
-document.addEventListener('click', function(e){
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active')
-    }
-})
- 
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
